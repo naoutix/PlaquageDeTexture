8eb4c3fcc1388f487a46087339f4cba1881d8827
Leo <jleo.dary@gmail.com>
2021-12-18 20:14:44 +0100

    fastamarching update

--- bin/placementPierre.py
+++ bin/placementPierre.py
@@ -3,8 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.core.fromnumeric import shape 
 from scipy import interpolate,ndimage,misc,sparse
+from math import inf,sqrt
+import skfmm
 
-
+### Importation des données
 image = np.load("arraySave/image.npy")
 mask = sparse.load_npz("arraySave/MaskChemin.npz")
 environnement = img = cv2.imread("arraySave/environnement.jpg")
@@ -12,10 +14,15 @@ ListeTaillePierre = np.load("arraySave/ListTaillePierre.npy" )
 Pierre_mask = np.load("arraySave/PierreMask.npy",allow_pickle=True)
 Pierre_sorted = np.load("arraySave/PierreSorted.npy",allow_pickle=True)
 
-res = ndimage.gaussian_filter(mask.toarray(),sigma =10,order=2)
-res = res[3000:4000,1000:2000]
-print(res.shape)
-np.savetxt("res",res,fmt="%d")
-fig = plt.figure()
+mask_array = -mask.toarray()+1
+#res = ndimage.gaussian_filter(mask.toarray(),sigma =10,order=2)
+
+## Fast marching
+res = skfmm.distance(mask_array,dx=1)
+k=0
+while k==2:
+    index = res.argmax()
+    k = k+1
+
 plt.imshow(res,cmap="gray",origin="lower")
 plt.show()
