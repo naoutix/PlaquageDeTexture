757e7cda1c33afcd442ff10269f04a5c5e735756
naoutix <jleo.dary@gmail.com>
2022-01-31 16:59:32 +0100

    3D

--- 3D/HeightMapPerlin_2D.py
+++ 3D/HeightMapPerlin_2D.py
@@ -1,15 +1,16 @@
 # Tutoriel: https://www.youtube.com/watch?v=aN05NVdiM8I
 
+import cv2
 import noise
 from PIL import Image, ImageDraw, ImageFilter, ImageOps
 import numpy as np
-
+import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import axes3d
 
 
-def set_color(x,y,image,value):
+def set_color(x,y,image,value,limite):
     """[summary]
 
     Args:
@@ -18,15 +19,15 @@ def set_color(x,y,image,value):
         image ([type]): [description]
         value ([type]): [description]
     """    
-    if value < -0.07:
+    if value < limite:
         image.putpixel((x,y), blue)
-    elif value < 0:
+    elif value < -0.1:
         image.putpixel((x,y), beach)
-    elif value < 0.25:
+    elif value < 0.10:
         image.putpixel((x,y), green)
-    elif value < 0.5:
+    elif value < 0.30:
         image.putpixel((x,y), mountains)
-    elif value < 1.0:
+    else:
         image.putpixel((x,y), snow)
 
 def set_texture(image,image_texture,couleur_texture):
@@ -52,13 +53,26 @@ def set_texture(image,image_texture,couleur_texture):
                 im_texture_finale.putpixel((x,y),im[x,y])
     return im_texture_finale
 
-## Constante
-shape = (512,512)
+def paste_texture(file,nb,shape,environment,color):
+    texture_elem = Image.open(file)
+    bg_w, bg_h = texture_elem.size
+    texture = Image.new('RGB', tuple(i*nb for i in shape ))
+    
+    w, h = texture.size
+    for i in range(0, w, bg_w):
+        for j in range(0, h, bg_h):
+            texture.paste(texture_elem, (i, j))
+    final = texture.resize((shape[0],shape[1]))
+    return set_texture(environment,final,color)
 
-scale = 50.0
-octaves = 2
+## Constante
+grandeur= 3
+shape = (512*grandeur,512*grandeur)
+
+scale = 100.0*grandeur
+octaves = 8
 lacunarity = 2.0
-persistence = 1.2
+persistence = 0.5
 
 # Couleur
 blue = (66,110,225)
@@ -67,10 +81,12 @@ beach = (240,210,172)
 mountains = (140,140,140)
 snow = (250,250,250)
 
-image_filepath = "noise.jpg"
+image_filepath = "noise.png"
 
 image = Image.new(mode="RGB",size=shape)
 
+values =np.zeros(shape)
+limite = -0.2
 for x in range(shape[0]):
     for y in range(shape[1]):
         value = noise.pnoise2(x/scale,
@@ -81,60 +97,33 @@ for x in range(shape[0]):
                             repeatx=shape[0],
                             repeaty=shape[1],
                             base=0)
-        set_color(x,y,image,value)
+        set_color(x,y,image,value,limite)
+        value = value-limite
+        if value<0:
+            value = 0
+        values[y,x] = value
+# max_arr = np.max(values)
+# min_arr = np.min(values)
+# norm_me = lambda x: (x)/(max_arr)
+# norm_me = np.vectorize(norm_me)
+# arr = norm_me(values)
+arr = np.round(values*np.round(255/(1-limite)))
+cv2.imwrite("perlin.png",arr)
 image.save(image_filepath)
 
 # Application de la texture sur chaque couleur correspondant à la texture
 # Mer:
-image_mer = Image.open("../2D/Textures/freeTexture4.png")
-bg_w, bg_h = image_mer.size
-mer = Image.new('RGB', (3000,3000))
-w, h = mer.size
-for i in range(0, w, bg_w):
-    for j in range(0, h, bg_h):
-        mer.paste(image_mer, (i, j))
-mer = mer.resize((shape[0],shape[1]))
-texture_finale = set_texture(image,mer,blue)
+texture_finale = paste_texture("../2D/Textures/freeTexture4.png",grandeur*2,shape,image,blue)
 # Herbe:
-image_herbe = Image.open("../2D/Textures/freeTexture2.png")
-bg_w, bg_h = image_herbe.size
-herbe = Image.new('RGB', (3000,3000))
-w, h = herbe.size
-for i in range(0, w, bg_w):
-    for j in range(0, h, bg_h):
-        herbe.paste(image_herbe, (i, j))
-herbe = herbe.resize((shape[0],shape[1]))
-texture_finale = set_texture(texture_finale,herbe,green)
+texture_finale = paste_texture("../2D/Textures/freeTexture2.png",grandeur*2,shape,texture_finale,green)
 # Plage:
-image_mer = Image.open("../2D/Textures/freeTexture1.png")
-bg_w, bg_h = image_mer.size
-mer = Image.new('RGB', (3000,3000))
-w, h = mer.size
-for i in range(0, w, bg_w):
-    for j in range(0, h, bg_h):
-        mer.paste(image_mer, (i, j))
-plage = mer.resize((shape[0],shape[1]))
-texture_finale = set_texture(texture_finale,plage,beach)
+texture_finale = paste_texture("../2D/Textures/freeTexture1.png",grandeur*2,shape,texture_finale,beach)
 # Montagne:
-image_mer = Image.open("../2D/Textures/freeTexture13.png")
-bg_w, bg_h = image_mer.size
-mer = Image.new('RGB', (3000,3000))
-w, h = mer.size
-for i in range(0, w, bg_w):
-    for j in range(0, h, bg_h):
-        mer.paste(image_mer, (i, j))
-montagne = mer.resize((shape[0],shape[1]))
-texture_finale = set_texture(texture_finale,montagne,mountains)
+texture_finale = paste_texture("../2D/Textures/freeTexture13.png",grandeur*2,shape,texture_finale,mountains)
 # Neige:
-image_mer = Image.open("./../2D/Textures/freeTexture15.png")
-bg_w, bg_h = image_mer.size
-mer = Image.new('RGB', (3000,3000))
-w, h = mer.size
-for i in range(0, w, bg_w):
-    for j in range(0, h, bg_h):
-        mer.paste(image_mer, (i, j))
-neige = mer.resize((shape[0],shape[1]))
-texture_finale = set_texture(texture_finale,neige,snow)
-texture_finale.save("texture_finale.jpg")
+texture_finale = paste_texture("../2D/Textures/freeTexture15.png",grandeur*2,shape,texture_finale,snow)
+plt.imshow(texture_finale)
+plt.show()
+texture_finale.save("texture_finale.png")
 
 # Mettre la carte de hauteur en 3D ou coller la texture avec les chemins sur la texture 3D
--- 3D/threejs/threejs/interface.js
+++ 3D/threejs/threejs/interface.js
@@ -1,4 +1,3 @@
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -6,16 +5,34 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-geometry = new THREE.BoxGeometry(3,2,1);
-material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-cube = new THREE.Mesh( geometry, material );
-//scene.add(cube);
 
+// geometry = new THREE.BoxGeometry(3,2,1);
+// material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// cube = new THREE.Mesh( geometry, material );
+//scene.add(cube);
+var img = new Image();
+var data_tab;
+img.src = 'perlin.png';
+img.onload = function() {
+	dessiner(this);
+  };
+function dessiner(img) {
+	const canvas = createElementNS('canvas');
+	canvas.height = img.height;
+	canvas.width = img.width;
+	var ctx = canvas.getContext('2d')
+	ctx.drawImage(img, 0,0);
+	data_tab = ctx.getImageData(0, 0, img.width, img.height);
+}
+// yo = canvas.getContext('2d');
+// yo.drawImage(img, 0, 0,img.width,img.height);
+// imagedata = yo.getImageData(0, 0, img.width, img.height);
+// imagedata.needsUpdate = true;
 
 // Geometry Plan
-geometry = new THREE.PlaneGeometry( 10, 10 ,10,10);
-texture = THREE.ImageUtils.loadTexture( "texture_finale.jpg" );
-material = new THREE.MeshLambertMaterial({ map : texture ,side:THREE.DoubleSide,wireframe:true});
+geometry = new THREE.PlaneGeometry( 10, 10 ,img.height-1,img.width-1);
+texture = THREE.ImageUtils.loadTexture( "texture_finale.png" );
+material = new THREE.MeshLambertMaterial({ map : texture ,side:THREE.DoubleSide,wireframe:false});
 plane = new THREE.Mesh( geometry, material );
 
 scene.add( plane );
@@ -34,17 +51,18 @@ function animate() {
 	//En boucle 60 fois par seconde:
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	//cube.rotation.x += 0.01;
+	//cube.rotation.y += 0.01;
 
 	controls.update();
-
 	renderer.render( scene, camera );
 };
 
 function randomize_elevation(value=0.2, offset=0) {
-    for (let i= 0; i < plane.geometry.attributes.position.length/3; i++) {
-        plane.geometry.attributes.position.setZ(i,Math.random()*value+offset);
+	error = value
+    for (let i= 0; i < plane.geometry.attributes.position.count; i++) {
+        //plane.geometry.attributes.position.setZ(i,Math.random()*value+offset);
+		plane.geometry.attributes.position.setZ(i,(data_tab.data[i*4]/255)*3);
     }
     plane.geometry.attributes.position.needsUpdate = true;
 }
